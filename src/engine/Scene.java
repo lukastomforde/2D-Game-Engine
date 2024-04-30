551752fabc20b21c2722d7bf8bcf8865c45ba926
Lukas Tomforde <tomfordelukas@gmail.com>
2024-04-30 06:51:02 +0200

    started object management system

--- src/engine/Scene.java
+++ src/engine/Scene.java
@@ -3,10 +3,13 @@ package engine;
 public abstract class Scene {
     
     protected Camera camera;
-    
+    private boolean isRunning = false;
+
     public Scene(){}
 
     public void init(){}
 
     public abstract void update(float dt);
+
+    
 }
---
+++ src/engine/objects/GameObject.java
@@ -0,0 +1,54 @@
+package engine.objects;
+
+import java.util.List;
+
+public class GameObject {
+    
+    private String name;
+    private List<Component> components;
+    
+    public GameObject(String name){
+        this.name = name;
+    }
+
+    public <T extends Component> T getComponent(Class<T> componentClass){
+        for (Component c : components){
+            if(componentClass.isAssignableFrom(c.getClass())){
+                try {
+                    return componentClass.cast(c);   
+                } catch (Exception e) {
+                    assert false : "Error: Failed casting component.";
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public <T extends Component> void removeComponent(Class<T> componentClass){
+        for (int i=0; i<components.size(); ++i){
+            Component c = components.get(i);
+            if (componentClass.isAssignableFrom(c.getClass())){
+                components.remove(i);
+                return;
+            }
+        }
+    }
+
+    public void addComponent(Component c){
+        components.add(c);
+        c.gameObject = this;
+    }
+
+    public void update(float dt){
+        for (Component c : components){
+            c.update(dt);
+        }
+    }
+
+    public void start(){
+        for (Component c : components){
+            c.start();
+        }
+    }
+}
